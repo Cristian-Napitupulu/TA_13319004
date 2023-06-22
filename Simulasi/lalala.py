b08085e35c72d078754224ffeac91430e93b8b42
Cristian-Napitupulu <13319004@mahasiswa.itb.ac.id>
2023-06-22 14:18:10 +0700

    lalala

--- Simulasi/lalala.py
+++ Simulasi/lalala.py
@@ -1,8 +1,4 @@
 import pandas as pd
-
 filename = 'data.csv'
 df = pd.read_csv(filename)
-
-new_row = {'A': 100, 'B': 200, 'C': 300}  # Example dictionary for a new row
-
-df = df.append(new_row, ignore_index=True)
+max_value = df['Column_Name'].max()
